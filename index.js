9f1e3862f70fca3a982f10a296c3f0242d40b4f5
Dustan Curtis <69445583+makerzenen@users.noreply.github.com>
2020-08-19 12:42:33 -0600

    Add basic server with logging, config (env)

---
+++ index.js
@@ -0,0 +1,73 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const http = require("http"),
+  path = require("path"),
+  methods = require("methods"),
+  express = require("express"),
+  bodyParser = require("body-parser"),
+  session = require("express-session"),
+  errorhandler = require("errorhandler"),
+  pino = require("pino"),
+  expressPino = require("express-pino-logger");
+
+const logger = pino({ level: process.env.LOG_LEVEL || "info" });
+const expressLogger = expressPino({ logger });
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(require("method-override")());
+app.use(express.static(__dirname + "/public"));
+app.use(
+  session({
+    secret: "conduit",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
+// Error handling.
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+app.use(function (req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+if (!isProduction) {
+  app.use(function (err, req, res, next) {
+    logger.error(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
+
+// Start server.
+const server = app.listen(process.env.PORT, () => {
+  logger.info(`Server is listening on port ${server.address().port}`);
+});
