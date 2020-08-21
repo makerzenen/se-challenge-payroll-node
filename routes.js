d8022655905f00b29439696e500d8fe9405164cc
Dustan Curtis <69445583+makerzenen@users.noreply.github.com>
2020-08-21 10:19:46 -0600

    Add csv parsing, file uploads, payroll report generation

--- routes.js
+++ routes.js
@@ -1,70 +1,80 @@
 const express = require("express"),
   multiparty = require("multiparty"),
-  csv = require("csv-file-validator");
-const logger = require("./logger.js");
+  csv = require("csv-parser"),
+  fs = require("fs");
+const logger = require("./logger.js"),
+  queries = require("./queries.js");
 
 const router = express.Router();
 
-// For CSV validation.
-const csvConfig = {
-  headers: [
-    {
-      name: "date",
-      inputName: "date",
-      required: true,
-      requiredError: (headerName, rowNumber, columnNumber) => {
-        return `${headerName} is required in the ${rowNumber} row : ${columnNumber} column.`
-      }
-    }, {
-      name: "hours worked",
-      inputName: "hoursWorked",
-      required: true,
-      requiredError: (headerName, rowNumber, columnNumber) => {
-        return `${headerName} is required in the ${rowNumber} row : ${columnNumber} column.`
-      }
-    }, {
-      name: "employee id",
-      inputName: "employeeId",
-      required: true,
-      requiredError: (headerName, rowNumber, columnNumber) => {
-        return `${headerName} is required in the ${rowNumber} row : ${columnNumber} column.`
-      }
-    }, {
-      name: "job group",
-      inputName: "jobGroup",
-      required: true,
-      requiredError: (headerName, rowNumber, columnNumber) => {
-        return `${headerName} is required in the ${rowNumber} row : ${columnNumber} column.`
-      }
-    }
-  ]
+const headers = ["date", "hoursWorked", "employeeId", "jobGroup"];
+
+function parsePayrollId(filename) {
+  const matches = filename.match(/\d+/);
+  if (matches) {
+    const id = matches[0];
+    logger.info(`Parsed payroll of ID: ${id}.`);
+    return id;
+  } else {
+    logger.error(`Unable to parse payroll ID from: ${filename}.`);
+    return false;
+  }
 }
 
 router.get("/", (req, res) => {
-  res.json({ message: "se-challenge-payroll homepage" });
+  res.json({ message: "se-challenge-payroll /" });
 });
 
 router.get("/ping", (req, res) => {
-  res.json({ message: "se-challenge-payroll ping" });
+  res.json({ message: "se-challenge-payroll /ping" });
+});
+
+router.get("/payroll-report", (req, res) => {
+  const payrollReport = queries.getPayrollReport();
+  res.json({ payrollReport: payrollReport });
 });
 
 router.post("/payroll-upload", (req, res) => {
   // Parse form for file upload.
   const form = new multiparty.Form();
   form.parse(req, function (err, fields, files) {
-    // 0. Validate CSV.
-    // 1. Parse and insert time report id.
-    // 2. Insert missing employees.
-    // 3. Insert payroll entries.
-    csv(files[0], csvConfig)
-      .then(csvData => {
-        logger.info("/payroll-upload", csvData.data);
-        logger.info("/payroll-upload", csvData.inValidMessages);
-      }).catch(err => {
-        logger.error("/payroll-upload");
-      })
-    res.json({ message: "File uploaded successfully." })
-  })
+    if (err) {
+      res.status(400).send({ message: "Invalid request.", error: err.message });
+      return;
+    }
+    const filename = files.file[0].originalFilename;
+    const filepath = files.file[0].path;
+    // Validate payroll ID from filename.
+    const fileId = parsePayrollId(files.file[0].originalFilename);
+    if (!fileId) {
+      res.status(400).send({ message: `Invalid filename: ${filename}.` });
+      return;
+    }
+    // Parse CSV rows into array.
+    const results = [];
+    const failedResults = [];
+    fs.createReadStream(filepath)
+      .pipe(csv({ strict: true }, headers))
+      .on("data", (data) => results.push(data))
+      .on("data", (data) => logger.debug(data))
+      .on("error", (error) => failedResults.push(error))
+      .on("error", (error) => logger.debug(error))
+      .on("end", () => {
+        logger.info(
+          `Payroll ID ${fileId} successfully processed ${
+            Object.keys(results).length
+          } entries.`,
+        );
+        if (failedResults > 0) {
+          logger.error(
+            `Payroll ID ${fileId} failed to process ${
+              Object.keys(failedResults).length
+            } entries.`,
+          );
+        }
+      });
+    res.json({ message: "File uploaded and processed successfully." });
+  });
 });
 
 module.exports = router;
