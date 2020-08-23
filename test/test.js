ae57773ed443c3469bc307c3ac675d096b8b8555
Dustan Curtis <69445583+makerzenen@users.noreply.github.com>
2020-08-22 20:58:34 -0600

    Test getPayPeriod()

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 const chai = require("chai"),
   sinon = require("sinon"),
-  request = require("supertest");
-const router = require("../routes.js"),
-  app = require("../index.js");
+  request = require("supertest"),
+  moment = require("moment");
+const app = require("../index.js"),
+  router = require("../routes.js"),
+  queries = require("../queries.js"),
+  logger = require("../logger");
 
 // Ensures the test suite is working.
 describe("baseline test", () => {
@@ -60,4 +63,26 @@ describe("POST /time-report-upload @file", () => {
         if (err) throw err;
       });
   });
+});
+
+describe("queries.getPayPeriod() returns correct pay period start and end dates", () => {
+  it("should validate pay period start and end dates", () => {
+    const dates = [
+      { date: "14/12/2001", expectedStart: "1/12/2001", expectedEnd: "15/12/2001" },
+      { date: "4/5/2002", expectedStart: "1/5/2002", expectedEnd: "15/5/2002" },
+      { date: "27/2/2016", expectedStart: "16/2/2016", expectedEnd: "29/2/2016" },
+      { date: "29/2/2020", expectedStart: "16/2/2020", expectedEnd: "29/2/2020" },
+    ];
+    dates.forEach(entry => {
+      let result = queries.getPayPeriod(entry.date);
+      let startDateParts = entry.expectedStart.split("/");
+      entry.expectedStart = moment(`${startDateParts[2]}-${queries.zeroPad(startDateParts[1], 2)}-${queries.zeroPad(startDateParts[0], 2)}`);
+      let endDateParts = entry.expectedEnd.split("/");
+      entry.expectedEnd = moment(`${endDateParts[2]}-${queries.zeroPad(endDateParts[1], 2)}-${queries.zeroPad(endDateParts[0], 2)}`);
+      console.log(entry.expectedStart);
+      console.log(entry.expectedEnd);
+      chai.expect(result.startDate.format("YYYY-MM-DD")).to.equal(entry.expectedStart.format("YYYY-MM-DD"));
+      chai.expect(result.endDate.format("YYYY-MM-DD")).to.equal(entry.expectedEnd.format("YYYY-MM-DD"));
+    });
+  })
 });
